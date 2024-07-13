8802feed267fd774cadc833cb55fccca439bd42e
Clinton Boys <clintonboys@icloud.com>
2024-07-13 14:00:49 +0100

    minor progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// Load election results or simulate election
+// Simulate results under chosen electoral system
+use chrono::prelude::*;
+
+pub struct ElectionResults{
+    overall_result: OverallElectionResult,
+    results_by_constituency: Map
+}
+
+pub struct Election{
+    date: DateTime<Utc>,
+    name: String,
+    results: ElectionResults,
+}
+
 fn main() {
     println!("Hello, world!");
 }
